137893287ae43252f12e67f0d2d40662e9b22d89
Eloá Machado Martines <eloamachadomartines1@gmail.com>
2026-04-23 08:33:04 -0400

    oi

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+
 import projectRoutes from './routes/projectsRoutes.js'
 const app = express()
+
 app.use(express.json())//Para o express lidra com json
+
 const port = 3030;
 
 app.use('/api/v1/projects', projectRoutes);
+
 
 app.get('/health', (req, res) =>{
     res.json({satus: "OK"})
